a597cd1167911a3cdc6d42f62e30106519bc27c0
Folke Lemaitre <folke.lemaitre@gmail.com>
2020-01-15 19:05:38 +0100

    feat: ✨ added semantic-release

---
+++ packages/devmoji/release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["lib", "README.md", "CHANGELOG.md", "bin", "package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+}
