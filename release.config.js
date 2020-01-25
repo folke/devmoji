578258bdc6151c36b62eee31529fe82dc898e1c8
Folke Lemaitre <folke.lemaitre@gmail.com>
2020-01-25 10:53:34 +0100

    ci(deps): 👷 automerge minor, patch, pin and digest

--- release.config.js
+++ release.config.js
@@ -7,25 +7,7 @@ module.exports = {
         releaseRules: [
           { type: "docs", release: "patch" },
           { type: "perf", release: "patch" },
-          { type: "style", release: "patch" },
         ],
-        parserOpts: {
-          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
-        },
-        presetConfig: {
-          types: [
-            { type: "feat", section: "Features" },
-            { type: "fix", section: "Bug Fixes" },
-            { type: "chore", section: "Other" },
-            { type: "docs", section: "Other" },
-            { type: "style", section: "Other" },
-            { type: "refactor", section: "Other" },
-            { type: "perf", section: "Other" },
-            { type: "test", section: "Other" },
-            { type: "build", section: "Other" },
-            { type: "ci", section: "Other" },
-          ],
-        },
       },
     ],
     [
@@ -37,7 +19,7 @@ module.exports = {
             { type: "feat", section: "Features" },
             { type: "fix", section: "Bug Fixes" },
             { type: "chore", section: "Other" },
-            { type: "docs", section: "Other" },
+            { type: "docs", section: "Documentation" },
             { type: "style", section: "Other" },
             { type: "refactor", section: "Other" },
             { type: "perf", section: "Other" },
@@ -50,11 +32,6 @@ module.exports = {
     ],
     "@semantic-release/changelog",
     "@semantic-release/npm",
-    [
-      "@semantic-release/git",
-      {
-        assets: [["CHANGELOG.md", "package.json", "../../CHANGELOG.md"]],
-      },
-    ],
+    "@semantic-release/git",
   ],
 }
